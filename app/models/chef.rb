28ae7ba3ed23267664143882c9192052a027b150
vtt01uk <vtt01uk@gmail.com>
2016-02-11 15:28:26 +0000

    Created tests for chefs model and validated them

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,2 +1,13 @@
 class Chef < ActiveRecord::Base
+	
+	#before-save callback; before saving emails, convert them to lowercase
+	before_save { self.email = email.downcase }
+	
+	validates :chefname , presence: true, length: {minimum: 3, maximum: 40}
+	# From http://emailregex.com/
+	VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	validates :email, presence: true, length: {maximum: 40},
+						uniqueness: { case_sensitive: false },
+	format: { with: VALID_EMAIL_REGEX }
+	
 end
