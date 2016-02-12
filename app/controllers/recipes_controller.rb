bba03b5996a4744b95e4808033aa96c39b6aaec9
vtt01uk <vtt01uk@gmail.com>
2016-02-12 12:47:40 +0000

    Added flash notification when creating new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,6 +10,18 @@ class RecipesController < ApplicationController
 	end
 		
 	def create
+#		binding.pry
+		@recipe = Recipe.new(recipe_params)
+		#hardcode chef_id until later on
+		@recipe.chef = Chef.find(1)
+		
+		if @recipe.save
+			#send notification
+			flash[:success] = "Your recipe was created successfully"
+			redirect_to recipes_path
+		else
+			render :new
+		end
 	end
 	
 	def edit
@@ -22,4 +34,10 @@ class RecipesController < ApplicationController
 		@recipe = Recipe.find(params[:id])
 	end
 	
+	#whitelist methods exclusive to this class
+	private
+		
+	def recipe_params
+		params.require(:recipe).permit(:name, :summary, :description)
+	end
 end
