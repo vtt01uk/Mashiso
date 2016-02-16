2ab7b2d5c23bccab4180226f4685e8d51f191dbf
vtt01uk <vtt01uk@gmail.com>
2016-02-16 13:36:24 +0000

    Modified chefs_controller to authenticate authorised users to edit recipes

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,5 +1,8 @@
 class ChefsController < ApplicationController
-
+	before_action :set_chef, only: [:edit, :update, :show]
+	#this action will be performed before all other actions!
+	before_action :require_same_user, only: [:edit, :update]
+	
 	def index
 		@chefs = Chef.paginate(page: params[:page], per_page: 3)
 	end
@@ -23,13 +26,14 @@ class ChefsController < ApplicationController
 	end
 	
 	def edit
-		@chef = Chef.find(params[:id])
+
+		#enforce chef can edit only their own recipes
 	end
 	
 	#will handle the the edit action
 	def update
 #		binding.pry
-		@chef = Chef.find(params[:id])
+
 		if @chef.update(chef_params)
 			flash[:success] = "Your profile has been updated successfully"
 			redirect_to recipes_path #show chef's page!
@@ -40,12 +44,23 @@ class ChefsController < ApplicationController
 	
 	
 	def show
-		@chef = Chef.find(params[:id])
+
 		@recipes = @chef.recipes.paginate(page: params[:page], per_page: 3)
 	end
 	
 	private
 	def chef_params
 		params.require(:chef).permit(:chefname, :email, :password)
+	end
+	
+	def set_chef
+			@chef = Chef.find(params[:id])
+	end
+	
+	def require_same_user
+		if current_user != @chef
+			flash[:danger] = "You can only edit your own profile"
+			redirect_to root_path
+		end
 	end
 end
